9d51e0d75a43bbaa1bec30968fd9aeeabda27b3e
Emery Cho <cho.e@live.com>
2017-11-08 09:28:02 -0800

    likely ready for code

---
+++ constructor-hangman.js
@@ -0,0 +1,52 @@
+var word = require("./construct-word.js");
+var letter = require("./constructor-letter.js");
+
+// pick random word (string)
+// blank string for input with length of how many letters in word
+// user guesses a letter
+// if letter exists, mark correct and fill in the blank
+// if not, guesses decrement and nothing else
+// repeat until game is over/complete
+// game over when guesses reach 0
+// game won when all blanks filled
+
+
+// put words into array
+// math.random to generate random number
+// number is used as index within array
+
+// use underscore for each letter in word
+// loop as many times as letters in word and add underscores each time
+
+// input letter from keyboard
+// if correct, print letter on the word's appropriate position
+// if not correct, display decremented number of guesses
+// determine if game is over
+
+// check if blank string has anymore underscores/blanks
+// if fails, check for amount of remaining guesses
+// when guesses reach 0, game is over
+
+
+
+
+// var word = ["one", "two", "three"]
+// var index = Math.floor(Math.random())
+// word[index]
+
+// var blanks = ""
+// for (i = 0; i < word.length; i++)
+// blanks += "_"
+
+// use inquirer to get user input
+// if (word.includes(input))
+// for (i = 0; i < word.length; i++)
+// if (word[i] == input)
+// blanks[i] = input
+// else 
+// guesses--
+// check for endGame
+
+// if (!blank.includes("_"))
+// else if (guesses == 0)
+
